3a01e8f519ee31def3b9405cc6416b14641d4058
abdelrahman-gad <abdelrahmangad95@gmail.com>
2021-03-14 14:39:33 +0200

    before applyin LSP

--- Players/ElhadaryChild.php
+++ Players/ElhadaryChild.php
@@ -6,8 +6,11 @@ class ElhadaryChild extends ElhadaryFather{
 
 
    
+    /**
+     * Liskove subistitution principle is that the child takes the same behaviour of the father "so you can substitute by the father insted of its children" 
+     */ 
     public function keeping(){
-        return true;
+        return " Good Goal Keeping ";
     }
 
 }
